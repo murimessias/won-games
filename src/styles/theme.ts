83123de62713d7acc0cd0fe942059fba3e136266
Murilo Messias <murilo.messias@gmail.com>
2021-08-24 10:43:29 -0300

    Add themes (Design System)

---
+++ src/styles/theme.ts
@@ -0,0 +1,50 @@
+export default {
+  grid: {
+    container: '130rem',
+    gutter: '3.2rem'
+  },
+  border: {
+    radius: '0.4rem'
+  },
+  font: {
+    family:
+      "Poppins, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    light: 300,
+    normal: 400,
+    bold: 600,
+    sizes: {
+      xs: '1.2rem',
+      sm: '1.4rem',
+      md: '1.6rem',
+      lg: '1.8rem',
+      xl: '2.0rem',
+      '2xl': '2.8rem'
+    }
+  },
+  colors: {
+    primary: '#F231A5',
+    secondary: '#3CD3C1',
+    mainBg: '#06092B',
+    white: '#FAFAFA',
+    black: '#030517',
+    lightGray: '#EAEAEA',
+    gray: '#8F8F8F',
+    darkGray: '#2E2F42'
+  },
+  spacings: {
+    '2xs': '0.8rem',
+    xs: '1.6rem',
+    sm: '2.4rem',
+    md: '3.2rem',
+    lg: '4.0rem',
+    xl: '4.8rem',
+    '2xl': '5.6rem'
+  },
+  layers: {
+    base: 10,
+    menu: 20,
+    overlay: 30,
+    modal: 40,
+    alwaysOnTop: 50
+  }
+}
